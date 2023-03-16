3616fa80a1cee135865b7169e12ea37a387b714f
takccc585 <114169693+takccc585@users.noreply.github.com>
2023-03-16 18:29:27 +0300

    Add files via upload

---
+++ laser.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include"settings.h"
+
+class Laser {
+private:
+	sf::Sprite sprite;
+	sf::Texture texture;
+	float speed = LASER_SPEED, speedx, speedy;
+public:
+	Laser(sf::Vector2f playerPos, int playerAngle) {
+		texture.loadFromFile(LASER_FILE_NAME);
+		sprite.setTexture(texture);
+		sprite.setPosition(playerPos);
+		sprite.setRotation(playerAngle);
+		speedx = speed * sin(playerAngle * 3.1415 / 180);
+		speedy = -speed * cos(playerAngle * 3.1415 / 180);
+	}
+	void update() {
+		sprite.move(speedx, speedy);
+	}
+
+	sf::Sprite& getSprite() {return sprite;}
+};
---
+++ settings.h
@@ -0,0 +1,9 @@
+#pragma once
+const float WINDOW_WIDTH = 1200;
+const float WINDOW_HEIGHT = 900;
+const float FPS = 60.f;
+float speed = 0.0;
+const std::string PLAYER_FILE_NAME = "spaceShips_002.png";
+const std::string LASER_FILE_NAME = "spaceShips_002.png";
+
+const float LASER_SPEED = 15.f;
